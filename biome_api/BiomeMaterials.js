dcf54fdada3c863417275f2b0e7fddfff05c2fb1
ImHungry1480 <al.amien060512@gmail.com>
2026-06-22 20:24:03 +0800

    Update BiomeMaterials.js

--- biome_api/BiomeMaterials.js
+++ biome_api/BiomeMaterials.js
@@ -1,5 +1,41 @@
-new BiomeMaterials(
-    "minecraft:grass_block",
-    "minecraft:dirt",
-    "minecraft:stone"
-)
+// biome_api/BiomeMaterials.js
+
+export class BiomeMaterials {
+
+    constructor(
+        topMaterial = "minecraft:grass_block",
+        middleMaterial = "minecraft:dirt",
+        bottomMaterial = "minecraft:stone"
+    ) {
+
+        this.topMaterial = topMaterial;
+        this.middleMaterial = middleMaterial;
+        this.bottomMaterial = bottomMaterial;
+
+    }
+
+    setTop(material) {
+        this.topMaterial = material;
+        return this;
+    }
+
+    setMiddle(material) {
+        this.middleMaterial = material;
+        return this;
+    }
+
+    setBottom(material) {
+        this.bottomMaterial = material;
+        return this;
+    }
+
+    toJSON() {
+
+        return {
+            top_material: this.topMaterial,
+            middle_material: this.middleMaterial,
+            bottom_material: this.bottomMaterial
+        };
+
+    }
+}
